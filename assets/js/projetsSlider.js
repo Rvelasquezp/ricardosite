d04b29b62bb06d720d75050661deed233c93c84a
Ricardo Velasquez <velasquezpinilla@gmail.com>
2026-05-07 16:24:09 -0400

    Add slider projets 100%

--- assets/js/projetsSlider.js
+++ assets/js/projetsSlider.js
@@ -20,9 +20,9 @@ export default function projetsSlider() {
 				watchSlidesProgress: true,
 				slideToClickedSlide: true,
 				breakpoints: {
-					640: { slidesPerView: 2.2, spaceBetween: 20 },
-					1024: { slidesPerView: 3.2, spaceBetween: 24 },
-					1400: { slidesPerView: 3.5, spaceBetween: 28 },
+					640: { slidesPerView: 1.5, spaceBetween: 20 },
+					1024: { slidesPerView: 2.5, spaceBetween: 24 },
+					1400: { slidesPerView: 2.5, spaceBetween: 28 },
 				},
 			});
 
